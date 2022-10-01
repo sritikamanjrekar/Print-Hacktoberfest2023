0009c4e14ee4b293bcb0d8d1349900d5529289c1
saurabhppawar04 <78255828+saurabhppawar04@users.noreply.github.com>
2022-10-02 03:39:48 +0530

    Myname
    Take name from user.
    Then print message.

---
+++ C/Myname.c
@@ -0,0 +1,11 @@
+#include<stdio.h>  
+void main()  
+{
+  char name[20];  
+  printf("Enter Your Name : "); 
+  scanf("%s",&name);  
+  printf("My name is %s ", name);
+  Printf("I am Contributing in Hacktober Open Source Project");
+  getch();   
+}
+

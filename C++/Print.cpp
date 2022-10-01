6abbde05be9755c260e3e9e1e57799c1a477fba4
Romijul Laskar <97734641+Romi904@users.noreply.github.com>
2022-10-01 19:28:59 +0530

    Update Print.cpp

--- C++/Print.cpp
+++ C++/Print.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main()
 {
     cout<<" My name is Kumar Utsav, I am Contributing in Hacktober Open Source Project"<<endl;
+    cout<<"Please accept me ,and merge me";
     return 0;
 }

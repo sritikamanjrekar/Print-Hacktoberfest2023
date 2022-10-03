04a4413114a5d111b73dc2dab4d767f28bbc73ff
Anirudh <59328945+anirudh0806@users.noreply.github.com>
2022-10-03 14:28:53 +0530

    Created batman.java

---
+++ Java/batman.java
@@ -0,0 +1,7 @@
+public class Hacktoberfest2022 {
+ 
+ public static void main(String[] args) {
+  System.out.println("My name is batman, I am Contributing in Hacktober Open Source Project");
+ }
+ 
+}

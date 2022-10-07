df0b121ca63d966dc85fe4ba61fb407ade13169e
Moksh Gupta <mokshgupta567@gmail.com>
2022-10-07 14:53:48 +0530

    delete

--- Java/my_name.java
+++
@@ -1,7 +0,0 @@
-public class my_name {
- 
-    public static void main(String[] args) {
-     System.out.println("My name is Moksh Gupta, I am Contributing in Hacktober Open Source Project");
-    }
-    
-   }

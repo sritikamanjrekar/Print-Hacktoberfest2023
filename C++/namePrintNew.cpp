db6417fa17c70dfad07cbe068889697199718de6
Hussain6 <workforhussain2@gmail.com>
2022-10-08 10:16:40 +0500

    Printing Hacktoberfest2022 in ruby has been added and the name of the contributer is also added in cp file in C++ folder

---
+++ C++/namePrintNew.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<" My name is Hussain, I am Contributing in Hacktober Open Source Project"<<endl;
+    return 0;
+}
---
+++ Ruby/PrintHacktoberfest2022.rb
@@ -0,0 +1,10 @@
+puts "My name is Hussain, I am Contributing in Hacktober Open Source Project \n\n\n"
+
+puts "Enter the number of times you want to print Hacktoberfest2022 : " 
+noOfTimesIteration = gets
+ noOfTimesIteration=noOfTimesIteration.to_i
+  for i in 0..noOfTimesIteration-1
+     puts "Hacktoberfest2022!";
+     end
+      
+return 0

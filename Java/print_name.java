4b685cbc710876bcf1917a104a1676b613888772
Madhurmoms <70679523+Madhur-Jatiya@users.noreply.github.com>
2022-10-15 20:08:01 +0530

    Create print_name.java

---
+++ Java/print_name.java
@@ -0,0 +1,5 @@
+public class print_name {
+    public static void main(String[] args) {
+      System.out.println("My name is Madhur Jatiya, I am Contributing in Hacktober Open Source Project");
+    }
+ }

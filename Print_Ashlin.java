dc0bdc2c4bbbde2439cf02153e9e81a69ae65735
19BCE7577 <ashlin.19bce7577@vitap.ac.in>
2022-10-15 09:48:16 +0530

    added my name in java

---
+++ Print_Ashlin.java
@@ -0,0 +1,5 @@
+public class Myname{
+	    public static void main(String args[]){
+		            System.out.println("*My name is Ashlin Dev and Contributing in Hacktoberfest open Source Project*!");
+			        }
+}

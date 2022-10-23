2960902b8ba6216a323f0852df73e7e9f7b486da
Shruti Chandak <99483160+shruti-2412@users.noreply.github.com>
2022-10-23 16:23:20 +0530

    Create print_myname.java

---
+++ Java/print_myname.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc= new Scanner(System.in);
+	    String name;
+	    System.out.println("Enter your name:");
+	    name= sc.next();
+		System.out.println("My name is "+name+" ,I am Contributing in Hacktober Open Source Project");
+	}
+}

e66df0f4d14a164f33da63b1654eaf1bf3c47e10
Jutika Patil <jutikapatiljutikapce120@gst.sies.edu.in>
2022-10-23 12:36:59 +0530

    added contribution in java

---
+++ Java/jutika.java
@@ -0,0 +1,8 @@
+package Java;
+
+public class jutika {
+  
+  public static void main(String args[]){
+    System.out.println("My name is Jutika and  I am Contributing in Hacktoberfest open Source Project!");
+  }
+}

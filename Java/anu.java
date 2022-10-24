bb6a32ad5deb23becbd092fadf89e9abafc2e269
anucodes <anuthedev@gmail.com>
2022-10-24 22:08:13 +0530

    Added name in anu.java

---
+++ Java/anu.java
@@ -0,0 +1,8 @@
+class anu{
+    static void printMsg(){
+        System.out.println("My name is Anu, I am Contributing in Hacktober Open Source Project");
+    }
+    public static void main(String args[]){
+        printMsg();
+    }
+}

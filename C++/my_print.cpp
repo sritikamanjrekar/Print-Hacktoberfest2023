0d05a1f25a63547363c983bd9eb86aa465706766
shivali0810 <shivaligupta0810@gmail.com>
2022-10-26 14:14:32 +0530

    added my_print.cpp

---
+++ C++/my_print.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"Hello Everyone"<< endl <<"This is Shivali Gupta"<<endl<<"I am contributng to Hacktoberfest 2022" <<endl;
+   return 0;
+}

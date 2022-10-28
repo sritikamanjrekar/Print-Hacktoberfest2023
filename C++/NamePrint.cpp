9de48f39ad8f038180ba82bd87c66e318d568bb2
Vishesh Gupta <64263308+guptavishu1000@users.noreply.github.com>
2022-10-28 10:53:11 +0530

    Added Name Print

--- C++/NamePrint.cpp
+++ C++/NamePrint.cpp
@@ -1,11 +1,9 @@
-
 #include<iostream>
 
 using namespace std;
 
 int main()
 {
-    cout<<" My name is Pakhi Vashishth, I am Contributing in Hacktober Open Source Project"<<endl;
+    cout<<" My name is Vishesh Gupta, I am Contributing in Hacktober Open Source Project"<<endl;
     return 0;
 }
-

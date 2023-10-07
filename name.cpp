6573c81a8337a2c065f5d11e5e98b4c7717c86a9
Superajay <95211492+Superajay@users.noreply.github.com>
2023-10-07 13:11:01 +0530

    Create name.cpp

---
+++ name.cpp
@@ -0,0 +1,7 @@
+My name is yourname, I am Contributing in Hacktober Open Source Project
+#include <iostream>
+
+int main() {
+	std::cout << "My name is Ajay, I am Contributing in Hacktober Open Source Project";
+	return 0;
+}

933163a1367d3cb58b1bf4dc259751b96a352716
him2016 <39089904+him2016@users.noreply.github.com>
2023-10-08 23:47:35 +0530

    Added Java code

---
+++ Java/PrintHimanshu.java
@@ -0,0 +1,5 @@
+public class PrintHimanshu {
+  public static void main(String args[]) {
+		  System.out.println("*My name is Himanshu and Contributing in Hacktoberfest Open Source Project");
+	}
+}

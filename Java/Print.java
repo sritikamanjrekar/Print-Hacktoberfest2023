6d79a24b3259323b71d36e423ab7ecdd4c325495
David <kerkettavishal777@gmail.com>
2023-10-08 21:18:43 +0530

    Added Print.java file

---
+++ Java/Print.java
@@ -0,0 +1,5 @@
+public class Print{
+    public static void main(String [] args ){
+        System.out.println("Hey there! I'm excited to share that I'm participating in Hacktoberfest this year to learn.");
+    }
+}

237e4664996d775fda1d3b9d50a1470c19ce161b
agni1402 <abhilash.agnihotri2003@gmail,com>
2023-10-09 11:23:28 +0530

    Added print_Abhilash.java

---
+++ Java/print_Abhilash.java
@@ -0,0 +1,5 @@
+public class print_Abhilash{
+    public static void main(String[] args) {
+        System.out.println("My name is Abhilash Agnihotri, I am Contributing in Hacktober Open Source Project");
+    }
+}

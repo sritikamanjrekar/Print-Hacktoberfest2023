620a56aa5d41571dc5a14ab9cfc05b44dc802784
aialok <alokgupta1560@gmail.com>
2023-10-11 13:52:30 +0530

    -Added my name in cpp

---
+++ C++/1Alok.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<" My name is Alok Gupta, I am Contributing in Hacktober Open Source Project :)"<<endl;
+    cout<<"Hey I really like your portfolio webiste. It is really to good. Enjoy"<<endl;
+    return 0;
+}

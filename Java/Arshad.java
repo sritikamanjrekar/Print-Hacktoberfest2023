6f0b73a560d0ffdca9f95e6cbb7d2399877535c2
Arshad Mahemood Patel <arshad.amp432@gmail.com>
2023-10-14 18:57:03 +0530

    Added my file in java

---
+++ Java/Arshad.java
@@ -0,0 +1,7 @@
+package Java;
+
+public class Arshad{
+    public static void main(String[] args) {
+        System.out.println("My name is yourname, I am Contributing in Hacktober Open Source Project");
+    }
+}

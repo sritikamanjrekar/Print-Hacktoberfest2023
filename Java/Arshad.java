ec2b88811a4b10ea21077814f3b3854b516c8390
Arshad Mahemood Patel <arshad.amp432@gmail.com>
2023-10-14 19:03:56 +0530

    Minor Corrections edit

--- Java/Arshad.java
+++ Java/Arshad.java
@@ -2,6 +2,6 @@ package Java;
 
 public class Arshad{
     public static void main(String[] args) {
-        System.out.println("My name is yourname, I am Contributing in Hacktober Open Source Project");
+        System.out.println("My name is Arshad M. Patel, I am Contributing in Hacktober 2023 Open Source Project");
     }
 }

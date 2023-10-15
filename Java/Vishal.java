cd57064ef4fcee18e77631d8e9fad293e8fd09cd
VishalAggarwal2 <136411362+VishalAggarwal2@users.noreply.github.com>
2023-10-15 22:19:29 +0530

    added my name vishal

---
+++ Java/Vishal.java
@@ -0,0 +1,7 @@
+package Java;
+
+public class Vishal {
+    public static void main (String[]args){
+        System.out.print("My name is Vishal, I am Contributing in Hacktober Open Source Project");
+    }
+}

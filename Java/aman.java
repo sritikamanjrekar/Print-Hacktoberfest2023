4b35da0a8d3538f33773c3722ba179821037569d
Aman Yadav <ydvaaman@gmail.com>
2023-10-16 21:52:24 +0530

    added my name

---
+++ Java/aman.java
@@ -0,0 +1,5 @@
+public class myname{
+    public static void main(String[] args){
+        System.out.println("My name is Aman Kumar Yadav, I am Contributing in Hacktober Open Source Project");
+    }
+}

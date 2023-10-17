fe2325134fbf498fa83ade6e60728833ee7d545f
SHADIL AM <109820522+shadil-rayyan@users.noreply.github.com>
2023-10-17 19:55:10 +0530

    Create printmyname.java

---
+++ Java/printmyname.java
@@ -0,0 +1,7 @@
+package Java;
+
+public class printmyname {
+    public static void main(String[] args) {
+        System.out.println("HacktoberFest2023");
+        System.out.println("Hello My Name is Shadil AM");
+        System.out.println("Passionate Software Developer && Open Source Contribuater");

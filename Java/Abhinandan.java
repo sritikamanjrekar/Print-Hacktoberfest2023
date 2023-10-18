00ab6fe2a8e02e3fc47f5b1f7d0e83d78e956995
Abhinandan Raj <abhinandanraj199@gmail.com>
2023-10-18 21:27:39 +0530

    Create Abhinandan.java

---
+++ Java/Abhinandan.java
@@ -0,0 +1,5 @@
+public class Abhinandan{
+        public static void main (String[]args){
+            System.out.print("My Name is Abhinandan Raj, I am Contributing in Hacktober Open Source Project. ");
+        }
+    }

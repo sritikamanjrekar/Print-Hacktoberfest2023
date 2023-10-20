c20c9c2b4604b4e2ab5c41c0ca9c2f9540406b8b
yukthi96 <63551946+yukthi96@users.noreply.github.com>
2023-10-20 16:16:27 +0530

    Create UniqueFloydAlgorithmpo.java

---
+++ Java/UniqueFloydAlgorithmpo.java
@@ -0,0 +1,43 @@
+
+public class UniqueFloydAlgorithm {
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+        // Create cycle
+        head.next.next.next.next = head.next;
+        boolean hasCycle = hasCycle(head);
+        if (hasCycle) {
+            System.out.println("The linked list has a cycle.");
+        } else {
+            System.out.println("The linked list does not have a cycle.");
+        }
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}

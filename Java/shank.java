54c83eb83959cba0e893a29df39b95f43cf394ac
shank1729 <104124777+shank1729@users.noreply.github.com>
2023-10-22 08:17:28 +0000

    added code

---
+++ Java/shank.java
@@ -0,0 +1,8 @@
+package Java;
+
+public class Akshay {
+    public static void main(String[] args) {
+       
+        System.out.println("My name is Shankholi Ray, I am Contributing in Hacktober Open Source Project");
+    }
+}

36dba23c7b66a4761473c02b88caa593599ca6b4
unknown <prashikh2@gmail.com>
2023-10-23 12:27:50 +0530

    hactoberfest first commit

---
+++ Java/Prashik.java
@@ -0,0 +1,9 @@
+package Java;
+
+public class Prashik {
+    public static void main(String[] args) {
+        System.out.println("HacktoberFest2023");
+        System.out.println("Hello My Name is Prashik Hingaspure");
+        System.out.println("Passionate Software Developer | Java Developer | Open Source Enthusiast");
+    }
+}

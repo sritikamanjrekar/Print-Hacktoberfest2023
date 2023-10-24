7e761998c664992b3c3f0433141e1a8ff89b6ec4
Lipasa <lipasamundha@gmail.com>
2023-10-24 22:40:17 +0530

    added my name

---
+++ Java/Lipasa.java
@@ -0,0 +1,7 @@
+package Java;
+
+public class Lipasa {
+    public static void main(String[] args) {
+        System.out.println("My name is Lipasa Mundha, I am Contributing in Hacktober Open Source Project");
+    }
+}

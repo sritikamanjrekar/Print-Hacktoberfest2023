57b4d2e157b4c6505a5a3a4e358d871f00bf6adb
Ghovkg02 <ghovkg02@gmail.com>
2023-10-26 23:37:10 +0530

    New User Commit

---
+++ Java/NameG.java
@@ -0,0 +1,7 @@
+public class nameG
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hey, hope your day is going great! I am contributing in open source as a beginner.");
+    }
+}

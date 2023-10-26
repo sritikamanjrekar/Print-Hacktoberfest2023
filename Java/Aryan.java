7b4133c751cd4604f84941abf79b72f86d129bd0
Aryan Garg <99543379+jsrjhbaryan959@users.noreply.github.com>
2023-10-26 16:19:05 +0530

    Create Aryan.java

---
+++ Java/Aryan.java
@@ -0,0 +1,7 @@
+public class Aryan {
+
+    public static void main(String[] args) {
+        System.err.println("My name is Aryan Garg, I am Contributing in Hacktoberfest 2023 Open Source Project");
+    }
+    
+}

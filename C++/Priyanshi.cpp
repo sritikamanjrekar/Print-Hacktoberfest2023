d1b8d0450bb251b30da0556d77e23c2160212175
24Priyanshi <24priyanshimishra@gmail.com>
2023-10-27 19:23:14 +0530

    Created a file called Priyanshi.cpp in c++ folder

---
+++ C++/Priyanshi.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"My name is Priyanshi Mishra.It's my first time participating in hacktoberfest. Please merge my pull request. Thanks for creating such beginner friendly repository!"<<endl;
+    return 0;
+}
